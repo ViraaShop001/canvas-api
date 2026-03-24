6a762401ff509c7a73c589ba2baf9b43b41932ae
ViraaShop001 <ranggaraditiawan9@gmail.com>
2026-03-24 08:24:49 +0700

    Update fakegopay.js

--- routes/fakegopay.js
+++ routes/fakegopay.js
@@ -1,7 +1,7 @@
+const { createCanvas, loadImage, registerFont } = require("canvas")
 const axios = require("axios")
 const fs = require("fs")
 const path = require("path")
-const { Canvas, loadImage, FontLibrary } = require("skia-canvas")
 
 module.exports = async (req,res)=>{
 
@@ -11,14 +11,12 @@ const nominal = req.query.nominal
 
 if(!nominal){
 return res.json({
-creator:"PusatAPI",
 status:false,
-error:"Masukkan nominal"
+error:"nominal required"
 })
 }
 
-const angkaInput = nominal.replace(/[^0-9]/g,"")
-const angka = Number(angkaInput).toLocaleString("id-ID")
+const angka = Number(nominal).toLocaleString("id-ID")
 
 const fontDir = path.resolve("./lib")
 
@@ -26,7 +24,7 @@ if(!fs.existsSync(fontDir)){
 fs.mkdirSync(fontDir,{recursive:true})
 }
 
-const fontPath = path.join(fontDir,"GopayFontV2.ttf")
+const fontPath = path.join(fontDir,"GopayFont.ttf")
 
 if(!fs.existsSync(fontPath)){
 
@@ -39,33 +37,21 @@ fs.writeFileSync(fontPath,Buffer.from(font.data))
 
 }
 
-FontLibrary.use("GopayFontV2",fontPath)
+registerFont(fontPath,{family:"GopayFont"})
 
 const bg = await loadImage("https://i.ibb.co/t6xRFvK/image.jpg")
-const arrow = await loadImage("https://files.catbox.moe/enqxau.png")
 
-const canvas = new Canvas(bg.width,bg.height)
+const canvas = createCanvas(bg.width,bg.height)
 const ctx = canvas.getContext("2d")
 
 ctx.drawImage(bg,0,0)
 
 ctx.fillStyle="#000"
-ctx.textBaseline="top"
 
-ctx.font="62px GopayFontV2"
-ctx.fillText("Rp",79,698)
+ctx.font="65px GopayFont"
+ctx.fillText("Rp "+angka,134,698)
 
-ctx.font="65px GopayFontV2"
-ctx.fillText(angka,134,698)
-
-const textWidth = ctx.measureText(angka).width
-
-const arrowX = 134 + textWidth + 44
-const arrowY = 694
-
-ctx.drawImage(arrow,arrowX,arrowY)
-
-const buffer = await canvas.png
+const buffer = canvas.toBuffer("image/png")
 
 res.setHeader("Content-Type","image/png")
 res.send(buffer)
@@ -73,7 +59,6 @@ res.send(buffer)
 }catch(e){
 
 res.json({
-creator:"PusatAPI",
 status:false,
 error:e.message
 })
